ffd292b1eab0fedbde058f4c883318e285f2b623
Sobocles <smoralespincheira@gmail.com>
2023-01-09 00:13:52 -0300

    validar correo y contrasena

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,11 @@
+import { validationResult } from 'express-validator'
+
 export const register = (req, res) => {
+    const errors = validationResult(req) //si no es un email va a viajar a traves del request ese mensaje de error y lo vamos a guardar en esa constante
+    
+    if(!errors.isEmpty()){ //si el usuario ingreso algo erroneo salta el mensaje
+        return res.status(400).json({ errors: errors.array() }); 
+    }
     console.log(req.body)
     res.json({ ok: true})
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,28 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
+import { body } from "express-validator";
 const router = express.Router();
 
-router.post('/register', register );
+router.post(
+    "/register",
+    [
+        body("email", "Formato de email incorrecto")
+            .trim()
+            .isEmail()
+            .normalizeEmail(), 
+        body("password", "Minimo 6 caracteres").trim().isLength({ min: 6}), //en el body se recibe una propiedad llamada email (el que llega del front)
+        body("password", "formato de password incorrecta").custom(
+            (value, { req }) => { //el value es el password
+                if (value !== req.body.repassword) {
+                    throw new Error("No coinciden las contraseñas"); //error personalizado
+                }
+                return value;
+            }
+        ), 
+    ],     
+    register  
+                                                                     //ese mail tiene que pasar ciertas validaciones, por ejemplo que sea una email en caso de que no sea deberia fallar (salta em mensaje)
+);
 router.post("/login", login);
 
 export default router;
