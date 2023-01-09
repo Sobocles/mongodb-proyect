1ecbe9bb15dedfbd79f6d6eac0d8a8837106f31a
Sobocles <smoralespincheira@gmail.com>
2023-01-08 22:40:11 -0300

    base de datos conectada con json

---
+++ controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const register = (req, res) => {
+    console.log(req.body)
+    res.json({ ok: true})
+}
+
+export const login = (req, res) => {
+    res.json({ ok: "login"});
+}
+
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import 'dotenv/config'
 import "./database/connectdb.js";
 import express from "express";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.json({ ok: true});
-});
+app.use(express.json()); //se habilita para que pueda recibir las solicitudes en json
+app.use("/api/v1", authRouter);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log("💥💢💌💌💥 http://localhost:" + PORT));
